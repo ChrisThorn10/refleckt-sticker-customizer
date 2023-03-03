5b262ba40dcdc725dc2b2825e156ebcc9239c11a
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2023-03-02 22:04:36 -0800

    create json feed. play around with importing svg dynamically.

--- src/blocks/Artboard.js
+++ src/blocks/Artboard.js
@@ -1,26 +1,27 @@
 import React from "react"
 import Svg from './Svg'
 import ColorPicker from './ColorPicker'
-import "../styles/Artboard.scss";
+import "../styles/Artboard.scss"
+import data from "../data.json"
+
+const colors = data.image.colors;
+let colorList=[];
+colors.forEach((item,index)=>{
+    colorList.push( 
+        <ColorPicker 
+            id={item.id} 
+            color={item.hexCode} 
+            label={item.label}
+        />
+    )
+})
+
 
 export default function Artboard(props) {
     return (
         <div>  
-            <Svg 
-                hairColor = "#000000"
-                skinColor = "#824836" 
-                />
-            <ColorPicker/>
-
-            {/* 
-            Need a json object that contains 
-                svg data
-                color profiles (for each print a color picker)
-                    hair color, skin color, other color (for each loop)
-                        hex code
-                        id
-                        label
-            */}
+            <Svg renderData={data.image.svgID} />
+            {colorList}
         </div>
     )
 }
