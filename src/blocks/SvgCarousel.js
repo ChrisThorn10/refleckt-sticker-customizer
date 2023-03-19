0edd4bb33ee4db384639a7ba584baf1dc01853a6
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2023-03-18 19:08:11 -0700

    add color variety to thumbnails and reposition component

--- src/blocks/SvgCarousel.js
+++ src/blocks/SvgCarousel.js
@@ -4,12 +4,14 @@ import Svg from "./Svg";
 
 
 export default function SvgCarousel(props) {
-    let svgList=[];
+    let svgList = [];
+    let randSkinColor = ["#3B2F2B", "#DFC9B3", "#703915", "#A57250"]
+    let randHairColor = ["#000000", "#543821", "#4E0E24", "#D1D1D1", "#D6C44C", "#08125E"]
 
     props.data.image.forEach((item,index)=>{
         svgList.push( 
             <div class="svg-item" onClick={props.onClick.bind(this, index)}>
-                <Svg svgID={item.svgID} newColor={["#000000","#824836"]}/>
+                <Svg svgID={item.svgID} newColor={[randHairColor[index%6],randSkinColor[index%4]]}/>
             </div>
         )
     })
