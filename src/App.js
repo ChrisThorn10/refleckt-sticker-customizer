bf611800f6d204beffc3f17688f9b023ee21530e
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2023-03-21 21:11:56 -0700

    add a logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Artboard from './blocks/Artboard';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src="logo4.svg" alt="refleckt" style={{"width": 250, "height": "auto"}}/>
+        </header>
       <Artboard />
     </div>
   );
