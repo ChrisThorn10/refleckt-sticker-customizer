19cf43827064b7a53b1c7a6eb20df88e5bba55d3
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2023-03-23 17:04:54 -0700

    more minor updates

--- src/svgs/peace0001.js
+++ src/svgs/peace0001.js
@@ -4,8 +4,8 @@ import React from "react";
 const Peace0001 = (props) => {
 
     return (
-        <svg viewBox="0 0 460 460" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                <title>Group</title>
+        <svg width="400px" height="400px" viewBox="0 0 460 460" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" role="img" aria-label={props.title}>
+                <title>{props.title}</title>
                 <desc>Created with Sketch.</desc>
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                     <g id="meditate">
